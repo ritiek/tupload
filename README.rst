35aa7c71e4e4a9991652079c1ca935e51d1baa49
ritiek <ritiekmalhotra123@gmail.com>
2017-09-16 11:43:21 +0530

    Cook setup.py

---
+++ README.rst
@@ -0,0 +1,7 @@
+tupload
+=======
+
+|Build Status|
+
+.. |Build Status| image:: https://travis-ci.org/ritiek/tupload.svg?branch=master
+   :target: https://travis-ci.org/ritiek/tupload
---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import tupload
+
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
+setup(name='tupload',
+      version=tupload.__version__,
+      description='Telegram bot to fetch files from the server to the client',
+      long_description=long_description,
+      author='Ritiek Malhotra',
+      author_email='ritiekmalhotra123@gmail.com',
+      packages=find_packages(),
+      entry_points={
+            'console_scripts': [
+                  'tupload = tupload.tupload:command_line',
+            ]
+      },
+      url='https://www.github.com/ritiek/tupload',
+      keywords=['telegram', 'bot', 'upload', 'download', 'files'],
+      license='MIT',
+      download_url='https://github.com/ritiek/tupload/archive/v' + tupload.__version__ + '.tar.gz',
+      classifiers=[],
+      install_requires=[
+            'requests',
+            'telepot',
+      ]
+     )

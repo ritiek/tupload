c2c2b8a28f25f71986d046f35a4774d17f0dd5f3
ritiek <ritiekmalhotra123@gmail.com>
2017-09-17 11:48:29 +0530

    Support all possible types of media

--- tupload/tupload.py
+++ tupload/tupload.py
@@ -20,23 +20,67 @@ def get_arguments():
 
 
 def handle(msg):
+    #print(msg)
     chat_id = msg['chat']['id']
+    bot.sendChatAction(chat_id, 'typing')
 
-    if 'document' in msg:
-        bot.sendChatAction(chat_id, 'typing')
-        file_name = msg['document']['file_name']
-        file_id = msg['document']['file_id']
+    downloadable = ['document, photo, video, audio, voice']
+    ignore = ['chat', 'date', 'from', 'message_id']
 
-        bot.sendMessage(chat_id, 'Downloading ' + file_name)
-        local_path = os.path.join(local_directory, file_name)
-        bot.download_file(file_id, local_path)
+    for item in list(msg):
+        if not item in ignore:
+            msg_type = item
+            break
 
-    else:
-        bot.sendChatAction(chat_id, 'typing')
+    if msg_type == 'text':
+        result = msg['text']
         bot.sendMessage(chat_id, "Talk is cheap. Send me the files.")
 
-    print('')
-    print(time.strftime('[%d %b, %y %r] ' + str(chat_id)) + ' : ' + str(file_name))
+    elif msg_type == 'document':
+        result = msg[msg_type]['file_name']
+        file_id = msg[msg_type]['file_id']
+
+    elif msg_type == 'photo':
+        result = str(msg['date']) + '.jpg'
+        file_id = msg[msg_type][0]['file_id']
+
+    elif msg_type == 'video':
+        file_type = msg[msg_type]['mime_type'].split('/')[-1]
+        result = str(msg['date']) + '.' + file_type
+        file_id = msg[msg_type]['file_id']
+
+    elif msg_type == 'audio':
+        file_type = msg[msg_type]['mime_type'].split('/')[-1]
+        result = msg['audio']['title'] + '.' + file_type
+        file_id = msg[msg_type]['file_id']
+
+    elif msg_type == 'voice':
+        file_type = msg[msg_type]['mime_type'].split('/')[-1]
+        result = str(msg['date']) + '.' + file_type
+        file_id = msg[msg_type]['file_id']
+
+    elif msg_type == 'contact':
+        name = msg['contact']['first_name']
+        number = msg['contact']['phone_number']
+        result = name + ' (' + number + ')'
+
+    elif msg_type == 'location':
+        result = '{0}, {1}'
+        result = result.format(msg['location']['latitude'], msg['location']['longitude'])
+
+    else:
+        result = 'This media type is not currently supported'
+
+    if msg_type in downloadable:
+        bot.sendMessage(chat_id, 'Downloading ' + result)
+        local_path = os.path.join(local_directory, result)
+        bot.download_file(file_id, local_path)
+
+    current_time = time.strftime('[%d %b, %y %r]')
+    string = '\n' + current_time + ': '
+    string += str(chat_id) + ' (' + msg['from']['username'] + '):'
+    string += '\n' + msg_type + ': ' + result
+    print(string)
 
 
 def command_line():
@@ -54,7 +98,7 @@ def command_line():
         global bot
         bot = telepot.Bot(token)
         bot.message_loop(handle)
-        print('Bot is ready to receive files...')
+        print('Bot is online')
 
         while True:
             time.sleep(10)
